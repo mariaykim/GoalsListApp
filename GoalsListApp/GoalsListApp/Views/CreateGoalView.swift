c2da40144a174ce6e80ef3dd78ce9bc529424495
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-09-10 01:16:45 -0400

    support for notifications

--- GoalsListApp/GoalsListApp/Views/CreateGoalView.swift
+++ GoalsListApp/GoalsListApp/Views/CreateGoalView.swift
@@ -63,6 +63,9 @@ struct CreateGoalView: View {
     
     func saveGoal() {
         if goalValid() {
+            DispatchQueue.main.async {
+                scheduleTimeBasedNotification()
+            }
             viewModel.addGoal(title: titleTextFieldText, description: descriptionTextFieldText, date: dateTextFieldText)
             alertTitle = "SUCCESS"
             showAlert.toggle()
@@ -77,6 +80,39 @@ struct CreateGoalView: View {
             return false
         }
         return true
+    }
+    
+    func scheduleTimeBasedNotification() {
+        // 1. Request permission to display alerts and play sounds.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { granted, error in
+            if granted {
+                print("Permission granted")
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+
+        // 2. Create the content for the notification
+        let content = UNMutableNotificationContent()
+        content.title = "Reminder"
+        content.body = "Don't forget to check the app!"
+        content.sound = UNNotificationSound.default
+
+        // 3. Set up a trigger for the notification
+        // For example, 5 seconds from now
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: (5), repeats: false)
+
+        // 4. Create the request
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+        // 5. Add the request to the notification center
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Notification scheduled")
+            }
+        }
     }
     
 }
